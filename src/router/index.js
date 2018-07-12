c8f007fe9f796c1cf596c7e8fb35fc8c5be3276c
李楠 <linan@chalco-steering.com>
2018-07-12 16:27:49 +0800

    尝试路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import menuLeft from '../pages/menu/menuLeft'
+import login from '../pages/login/login'
+import dragAndDrop from '../pages/demo/dragAndDrop'
+import l10 from '../pages/menu/111'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/helloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/menuLeft',
+      name: 'menuLeft',
+      component: menuLeft
+    },
+    {
+      path: '/',
+      name: 'login',
+      component: login
     }
+
   ]
 })
